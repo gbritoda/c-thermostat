0b8ff938ce16c46d374e225e12ed0321fe35b657
gbritoda <33906448+gbritoda@users.noreply.github.com>
2026-09-16 19:01:37 +0100

    A note on simulation

--- src/main.c
+++ src/main.c
@@ -3,11 +3,13 @@
 
 int main(void) {
     // 1. Initialize hardware peripherals and controller state
+    // TODO: I want the HAL simulation to be more lower level
+    // Instead of just printing to stdout maybe a simulation of PWM or something
     HAL_Init();
     Thermostat_Init();
 
-    // 2. Super-loop
     float current_temp;
+    // 2. Super-loop
     while (HAL_ReadTemperature(&current_temp)) {
         bool heater_cmd = Thermostat_ComputeControl(current_temp);
         if (Thermostat_IsFaulted()) {
